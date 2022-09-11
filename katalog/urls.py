8f7fe0a0aaaa5202d9de251f312039fcd7f200b2
Alfredo <alfredo.austin@ui.ac.id>
2022-09-11 14:48:51 +0700

    Finishing code of assignment 2 (without test)

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import render_view
+
+# app_name = "katalog"
+
+urlpatterns = [
+    path("", render_view, name="render_view"),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+
+
+def render_view(request):
+    list_katalog = CatalogItem.objects.all()
+    rendered_fields = {
+        "list_field": list_katalog,
+        "name": "Alfredo Austin",
+        "student_id": "2106705865",
+    }
+    return render(request, "katalog.html", rendered_fields)
